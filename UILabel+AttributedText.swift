f4728660228cc2d5406ad3818df97b81e4ce03e1
Rinat Enikeev <rinat.enikeev@gmail.com>
2014-12-09 02:47:33 +0500

    change method name to all substrings

--- UILabel+AttributedText.swift
+++ UILabel+AttributedText.swift
@@ -1,21 +1,13 @@
-//
-//  UILabel+AttributedText.swift
-//  Languagebox
-//
-//  Created by Rinat Enikeev on 09/12/14.
-//  Copyright (c) 2014 Languagebox. All rights reserved.
-//
-
 import Foundation
 
 public extension UILabel {
     
-    public func at_setWordColor(word : String?, color: UIColor?) {
-        if word != nil && color != nil {
+    public func at_setAllSubstringsColor(substring : String?, color: UIColor?) {
+        if substring != nil && color != nil {
             let attributedString = NSMutableAttributedString(attributedString: self.attributedText)
             let text = attributedString.mutableString
             
-            let expression = NSRegularExpression(pattern: "(" + word! + ")", options: NSRegularExpressionOptions.allZeros, error: nil)
+            let expression = NSRegularExpression(pattern: "(" + substring! + ")", options: NSRegularExpressionOptions.allZeros, error: nil)
             
             expression?.enumerateMatchesInString(text, options: NSMatchingOptions.allZeros, range: NSMakeRange(0, text.length), usingBlock: { (result: NSTextCheckingResult!, flags: NSMatchingFlags, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
                 
